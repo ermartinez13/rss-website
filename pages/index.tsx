612859e9664d145fb765b92a3516b3e96e15c91b
Erick Martinez <ermartinez13@gmail.com>
2023-01-02 05:41:04 -0500

    format files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Rey Software Solutions</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
     </>
-  );
+  )
 }
