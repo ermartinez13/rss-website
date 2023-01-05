580e52d83b79811ee38c48d35146be79d1db4525
Erick Martinez <ermartinez13@gmail.com>
2023-01-05 12:58:12 -0500

    add overlay state to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import { MobileContent } from './MobileContent'
 
 export function Header() {
+  const [showOverlay, setShowOverlay] = useState(false)
+
   return (
     <header>
       <MobileContent />
