60e8bcb75eec6f5d2ed7bafc1c27db4848f22137
Erick Martinez <ermartinez13@gmail.com>
2023-01-07 08:42:29 -0500

    refactor: remove ref usage to set mobile overlay height

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,17 @@
-import { MouseEventHandler, useRef, useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 import { MobileContent } from './MobileContent'
 import { MobileOverlay } from './MobileOverlay'
 
 export function Header() {
   const [showOverlay, setShowOverlay] = useState(false)
-  const ref = useRef<HTMLDivElement | null>(null)
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
-    setShowOverlay(!showOverlay)
-    if (ref.current) {
-      ref.current.style.height = showOverlay ? '0' : '100vh'
-    }
+    setShowOverlay((state) => !state)
   }
 
   return (
-    <header className='relative z-50 bg-[#20084E]'>
-      <MobileOverlay ref={ref} />
+    <header className='relative bg-[#20084E]'>
+      <MobileOverlay showOverlay={showOverlay} />
       <MobileContent showOverlay={showOverlay} handleClick={handleClick} />
     </header>
   )
--- src/components/Header/MobileOverlay.tsx
+++ src/components/Header/MobileOverlay.tsx
@@ -1,11 +1,9 @@
-import { forwardRef } from 'react'
+export function MobileOverlay({ showOverlay }: { showOverlay: boolean }) {
+  const height = showOverlay ? 'h-screen' : 'h-0'
 
-// eslint-disable-next-line react/display-name
-export const MobileOverlay = forwardRef<HTMLDivElement, {}>((props, ref) => {
   return (
     <div
-      className='fixed z-25 inset-0 w-screen h-0 bg-[#20084E] duration-500 lg:hidden'
-      ref={ref}
+      className={`fixed inset-0 w-screen ${height} bg-[#20084E] duration-500 lg:hidden overflow-y-hidden`}
     ></div>
   )
-})
+}
