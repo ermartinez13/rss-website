6819d5c204bf614b8e96ac996d6475591f859fb2
Erick Martinez <ermartinez13@gmail.com>
2023-01-07 17:00:50 -0500

    fix: dynamic scrollbar shifts overlay content

--- src/components/Header/MobileOverlay.tsx
+++ src/components/Header/MobileOverlay.tsx
@@ -11,7 +11,7 @@ export function MobileOverlay({ showOverlay }: { showOverlay: boolean }) {
       className={`fixed z-10 inset-x-0 top-0 ${height} bg-[#20084E] duration-500 lg:hidden overflow-y-auto`}
     >
       <div
-        className={`relative h-screen pt-32 flex flex-col items-center justify-between ${translate} duration-500`}
+        className={`relative h-screen pl-[calc(100vw-100%)] pt-32 flex flex-col items-center justify-between ${translate} duration-500`}
       >
         <div className='w-full'>
           <nav>
