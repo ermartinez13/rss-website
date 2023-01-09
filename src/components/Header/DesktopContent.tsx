4f0e544d26702828381f8d4d07563f9cb693f8a6
Erick Martinez <ermartinez13@gmail.com>
2023-01-09 10:02:46 -0500

    configure desktop content spacing

--- src/components/Header/DesktopContent.tsx
+++ src/components/Header/DesktopContent.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from './ThemeToggle'
 
 export function DesktopContent() {
   return (
-    <div className='hidden lg:block  md:px-10 lg:max-w-[56.25rem] mx-auto'>
+    <div className='hidden lg:block  md:px-10 lg:max-w-[64rem] xl:max-w-[75rem] 2xl:max-w-[80rem] mx-auto'>
       <div className='flex py-6 justify-between items-center'>
         <Link href='/'>
           <span className='sr-only'>Rey Software Solutions</span>
@@ -12,15 +12,15 @@ export function DesktopContent() {
             <Logo height={61} width={94} />
           </span>
         </Link>
-        <div className='flex items-center'>
-          <button className=' fill-white  text-center hover:opacity-50'>
+        <div className='flex items-center gap-x-8'>
+          <button className='fill-white text-center hover:opacity-50'>
             <ThemeToggle height={52} width={52} />
           </button>
           <nav>
-            <ul className='flex'>
+            <ul className='flex gap-x-8'>
               <li>
                 <Link
-                  className='text-white text-center text-[1.75rem] hover:opacity-50'
+                  className='text-white text-center text-[1.375rem] hover:opacity-50'
                   href='#'
                 >
                   Español
@@ -28,7 +28,7 @@ export function DesktopContent() {
               </li>
               <li>
                 <Link
-                  className='px-3 pt-2 pb-2.5 border-[3px] border-[#5CFFE4] rounded-[2rem] text-white text-center text-[1.75rem] hover:bg-[#5CFFE4] hover:text-[#20084E]'
+                  className='px-3 pt-2 pb-2.5 border-[3px] border-[#5CFFE4] rounded-[2rem] text-white text-center text-[1.375rem] hover:bg-[#5CFFE4] hover:text-[#20084E]'
                   href='#'
                 >
                   Send Message
